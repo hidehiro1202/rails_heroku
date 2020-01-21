5cba20da65fb3b3d999716d7ac7f3032fff93990
hidehiro omuro <hidehiro.omuro@gmail.com>
2020-01-21 08:33:33 +0000

    rail 04 user

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  
+  VALID_PASSWORD_REGEX = /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 15 }
+  
+  
+  validates :email, presence: true
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+  
+  validates :password, length: { in: 8..32 }
+  
+  validates :password, format: {with: VALID_PASSWORD_REGEX}
+  
+  
+  
+  
+  has_secure_password
+end
