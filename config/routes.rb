1002d0df09e884471e71a07edfa4f9c567424cd0
hidehiro omuro <hidehiro.omuro@gmail.com>
2020-01-28 09:06:59 +0000

    rails 07

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
   
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete  '/favorites', to: 'favorites#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
